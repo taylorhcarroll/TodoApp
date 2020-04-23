2602979f6861280f35dad280f6459761240e9655
Taylor <taylorhcarroll1@gmail.com>
2020-04-23 08:50:35 -0700

    .

--- Todo/Todo/Controllers/HomeController.cs
+++ Todo/Todo/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Todo.Data;
 using Todo.Models;
 
@@ -14,41 +15,27 @@ namespace Todo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        public HomeController(ILogger<HomeController> logger)
         {
-            _context = context;
-            _userManager = userManager;
+            _logger = logger;
         }
 
-        // GET: TodoListItems
-        public async Task<ActionResult> Index()
+        public IActionResult Index()
         {
-            var user = await GetCurrentUserAsync();
-            var items = await _context.TodoItem
-                .Where(si => si.ApplicationUserId == user.Id)
-                .ToListAsync();
-
-            return View(items);
+            return View();
         }
-
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
 
         public IActionResult Privacy()
         {
             return View();
         }
 
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
-        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
